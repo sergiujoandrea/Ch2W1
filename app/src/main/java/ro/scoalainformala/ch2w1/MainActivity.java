82157f3b860cabc441611440d6fa1496b0da2910
sergiujoandrea <82173229+sergiujoandrea@users.noreply.github.com>
2021-05-16 20:21:47 +0300

    Initial commit

---
+++ app/src/main/java/ro/scoalainformala/ch2w1/MainActivity.java
@@ -0,0 +1,14 @@
+package ro.scoalainformala.ch2w1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
